57085f168d93d812baf9e75e8dee8206bbaafe23
re1yan <re1yanwork@gmail.com>
2019-08-22 23:53:46 +0900

    v0.0.4だとPyPIにアップロードできなかったのでv0.0.5にした。pycryptoからpycryptodomeに変更したのが、v0.0.3からの変更点。

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ["requests>=2.14.2", "pycryptodome>=3.8.2"]
 
 setup(
     name='lineworks',
-    version='0.0.4',
+    version='0.0.5',
     description='It is a package that implements the server API and the Talk Bot API among the LINE WORKS APIs.',
     url='https://pypi.org/project/lineworks',
     author='Rei Suzuki',
@@ -20,6 +20,6 @@ setup(
     ],
     install_requires=requires,
     classifiers=[
-        'Programming Language :: Python :: 3.7.3',
+        'Programming Language :: Python :: 3.7',
     ],
 )
